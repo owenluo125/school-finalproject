cd187f19a28b0b9458b1fb6f6ed1a84e7ac49416
r.mostaan <r.mostaan@share.epsb.ca>
2024-05-13 10:22:16 -0600

    Worked on windows

---
+++ Window.py
@@ -0,0 +1,90 @@
+"""
+Title: Windows
+Author: Radin and Owen
+Date-created: 5/13/2024
+"""
+import pygame
+
+class Color:
+    WHITE = (255, 255, 255)
+    GREY = (50, 50, 50)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    ORANGE = (255, 145, 0)
+    PURPLE = (175, 25, 255)
+    YELLOW = (225, 225, 30)
+
+
+class Window:
+    """
+    Create the window that will load for pygame
+    :return: None
+    """
+
+    def __init__(self, title, width=1000, height=700, fps=30):
+        # text that appears in the title bar
+        self.__title = title
+        # width of the window frame
+        self.__width = width
+        # height of the window frame
+        self.__height = height
+        # window dimensions
+        self.__dimension = (self.__width, self.__height)
+        # frames per second
+        self.__fps = fps
+        # background color
+        self.__bg_color = Color.GREY
+        # clock object that tracks time
+        self.__clock = pygame.time.Clock()
+        # base surface that all surfaces are overlaid on top
+        self.__screen = pygame.display.set_mode(self.__dimension)
+        # colors the screen surface
+        self.__screen.fill(self.__bg_color)
+        # sets the window caption with the title
+        pygame.display.set_caption(self.__title)
+
+    # Modifier Methods
+    def updateFrame(self):
+        """
+        This updates the window object based on the fps
+        :return:
+        """
+        # waits the appropriate amount of time based on FPS before finishing the loop
+        self.__clock.tick(self.__fps)
+        # updates the computer screen with the new frame
+        pygame.display.flip()
+
+    def clearScreen(self):
+        """
+        Fill the screen with the background color
+        :return:
+        """
+        self.__screen.fill(self.__bg_color)
+
+    # Accessor Methods
+    def getScreen(self):
+        return self.__screen
+
+    def getVirtualWidth(self):
+        return self.__width
+
+    def getVirtualHeight(self):
+        return self.__height
+
+
+if __name__ == "__main__":
+    pygame.init()
+
+    WINDOW = Window("template")
+
+    while True:
+        # pygame retrieves all inputs
+        for event in pygame.event.get():
+            # checks if the window is closed
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+        WINDOW.getScreen()
+        WINDOW.updateFrame()
